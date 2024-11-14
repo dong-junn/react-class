ca011cc3f2aba0d1e23cb5de888b454d91efff36
dong-junn <djl020326@gmail.com>
2024-11-15 01:27:51 +0900

    11주차 2번 과제 제출

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StyledBasic from "./component/week11/1/StyledBasic";
+import StyledComp from "./component/week11/2/StyledComp";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StyledBasic/>
+  <StyledComp/>
 );
 
 // If you want to start measuring performance in your app, pass a function
