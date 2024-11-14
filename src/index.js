143bbef797b4ba5dc692730997fabd6e71624960
dong-junn <djl020326@gmail.com>
2024-11-14 23:50:34 +0900

    10주체 과제 4번 제출

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StateParent from "./component/week10/3/StateParent";
+import StateForm from "./component/week10/4/StateForm";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StateParent/>
+  <StateForm/>
 );
 
 // If you want to start measuring performance in your app, pass a function
