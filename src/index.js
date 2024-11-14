72ddef0ff005a1eaabeaa540293aeada420cd40f
dong-junn <djl020326@gmail.com>
2024-11-15 02:18:23 +0900

    11주차 4번 과제 제출

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StyledComp from "./component/week11/2/StyledComp";
+import MaterialDrawer from "./component/week11/4/MaterialDrawer";
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StyledComp/>
+  <MaterialDrawer/>
 );
 
 // If you want to start measuring performance in your app, pass a function
